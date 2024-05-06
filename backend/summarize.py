23754d754d8571fdcb408ee1785671ecdf788f3d
jais33007 <jais33007@gmail.com>
2024-05-06 14:59:22 +0200

    Cahnge summarizer

--- backend/summarize.py
+++ backend/summarize.py
@@ -1,27 +1,60 @@
+# from flask import Flask, request, jsonify
+# import transformers
+# from transformers import pipeline
+# from flask_cors import CORS
+
+
+# app = Flask(__name__)
+# CORS(app, origins=['http://localhost:3000']) 
+
+
+# summarizer = pipeline("summarization")  # Initialize summarizer
+
+# @app.route('/summarize', methods=['POST'])
+# def summarize():
+#   """Summarizes a paragraph received in the request body."""
+#   data = request.get_json()
+#   paragraph = data.get('paragraph')
+
+#   try:
+#     summary = summarizer(paragraph, max_length=100, min_length=30, truncation=True)
+#     return jsonify({'summary': summary[0]['summary_text']})
+#   except Exception as e:
+#     return jsonify({'error': str(e)}), 500  # Return error message and status code
+
+# if __name__ == '__main__':
+#   app.run(debug=True)  # Run the Flask app in debug mode
+
 from flask import Flask, request, jsonify
-import transformers
-from transformers import pipeline
+import openai
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000']) 
+CORS(app, origins=['http://localhost:3000'])
 
-
-summarizer = pipeline("summarization")  # Initialize summarizer
+# Set your OpenAI API key here
+openai.api_key = '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'
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
-  """Summarizes a paragraph received in the request body."""
-  data = request.get_json()
-  paragraph = data.get('paragraph')
+    data = request.get_json()
+    paragraph = data.get('paragraph')
+    print (paragraph)
 
-  try:
-    summary = summarizer(paragraph, max_length=100, min_length=30, truncation=True)
-    return jsonify({'summary': summary[0]['summary_text']})
-  except Exception as e:
-    return jsonify({'error': str(e)}), 500  # Return error message and status code
+        # Use OpenAI ChatGPT API for summarization
+    response = openai.Completion.create(
+    engine="babbage-002" ,
+    prompt=paragraph,
+    max_tokens=70,  
+    temperature=0.5, 
+    stop=["\n"]
+    )
+    summary = response.choices[0].text
+    #print(summary)
+
+    return jsonify({'summary': summary})
+    # except Exception as e:
+    #     return jsonify({'error': str(e)}), 500  # Return error message and status code
 
 if __name__ == '__main__':
-  app.run(debug=True)  # Run the Flask app in debug mode
-
+    app.run(debug=True)  # Run the Flask app in debug mode
